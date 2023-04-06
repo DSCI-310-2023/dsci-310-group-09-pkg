a2f24def89b36111052a6634cb3ecbbc74a1b9b6
Jenna Le Noble <jenna_lenoble@hotmail.com>
2023-04-06 13:30:42 -0700

    added hist_plot test + editted description

--- tests/testthat/test-bar_graph.R
+++ tests/testthat/test-bar_graph.R
@@ -3,7 +3,7 @@ library(tidyverse)
 library(ggcheck)
 
 source(here::here("R/bar_graph.R"))
-source(here::here("tests/bar_graph_test_helper.R"))
+source(here::here("tests/helpers/bar_graph_test_helper.R"))
 
 #test for input variables
 test_that("Function can only accept certain data types for arguments", {
@@ -16,12 +16,12 @@ test_that("Function can only accept certain data types for arguments", {
     expect_error(bar_graph(test_data, x, y, "x", "y", "x",100))
 })
 
-#test for output variables 
+#test for output variables
 test_that("Checks the output of the graph", {
     plot <- bar_graph(test_data, test_data$x, test_data$y, "x", "y", "x", "Bar Graph Test")
     expect_true(is.ggplot(plot))
     expect_equal(xlab(plot)$label, xlab(test_data)$label)
     expect_equal(ylab(plot)$label, ylab(test_data)$label)
     expect_equal(ggtitle(plot)$label, ggtitle(test_data)$label)
-    
+
 })
