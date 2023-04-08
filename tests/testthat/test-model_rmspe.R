a33899ab3688be0bb860214048ba3678fe4a0b43
Jenna Le Noble <jenna_lenoble@hotmail.com>
2023-04-07 20:32:47 -0700

    added tests for model_rmspe() + fixed note for scatter_plot()

---
+++ tests/testthat/test-model_rmspe.R
@@ -0,0 +1,38 @@
+#helper data
+
+set.seed(100)
+test_split1 <- rsample::initial_split(mtcars)
+test_training1 <- rsample::training(test_split1)
+test_testing1 <- rsample::testing(test_split1)
+test_model1 <- linearmodel(recipes::recipe(mpg ~ hp + wt, test_training1), test_training1)
+var1 <- "mpg"
+
+#test input data
+
+testthat::test_that("Function can only accept a list for the model parameter", {
+  testthat::expect_error(model_rmspe("test_model1", test_training1, var1))
+  testthat::expect_error(model_rmspe(5, test_training1, var1))
+  testthat::expect_error(model_rmspe(c(1,0), test_training1, var1))
+}
+)
+
+testthat::test_that("Function can only accept a dataframe for the data parameter", {
+  testthat::expect_error(model_rmspe(test_model1, "test_training1", var1))
+  testthat::expect_error(model_rmspe(test_model1, 6, var1))
+  testthat::expect_error(model_rmspe(test_model1, c(1,2,3), var1))
+}
+)
+
+testthat::test_that("Function can only accept a string for var parameter", {
+  testthat::expect_error(model_rmspe(test_model1, test_training1, mpg))
+  testthat::expect_error(model_rmspe(test_model1, test_training1, c(1,2,3)))
+  testthat::expect_error(model_rmspe(test_model1, test_training1, list(1,2,3)))
+  testthat::expect_error(model_rmspe(test_model1, test_training1, 6))
+}
+)
+
+#test for output variables
+testthat::test_that("Checks that the output is numeric", {
+  rmspe1 <- model_rmspe(test_model1, test_training1, var1)
+  testthat::expect_true(is.numeric(rmspe1))
+})
