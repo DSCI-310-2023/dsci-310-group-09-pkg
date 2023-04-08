22e79ba5a4d98c4adf0a73f34d3420929ec4a6ad
Vera Danilova <veradanilova765@gmail.com>
2023-04-07 22:33:15 -0700

    began adding tests

--- tests/testthat/test-scatter_plot.R
+++ tests/testthat/test-scatter_plot.R
@@ -6,12 +6,20 @@ test_data_scatter2 <- data.frame(x = c("blue", "red", "yellow"), y = c(3,2,1))
 
 test_data_scatter3 <- data.frame(x = c(1,1,1), y = c("yes", "no", "yes"))
 
-helper_scatter <- ggplot2::ggplot(test_data_scatter,ggplot2::aes(x=x,y=y)) +
+helper_scatter_empty_type <- ggplot2::ggplot(test_data_scatter,ggplot2::aes(x=x,y=y)) +
   ggplot2::geom_point()+
   ggplot2::labs(x="xlab",y="ylab")+
   ggplot2::ggtitle("Scatterplot Test")+
   ggplot2::theme(text=ggplot2::element_text(size=10))
-helper_scatter
+helper_scatter_empty_type
+
+helper_scatter_lm_type <- ggplot2::ggplot(test_data_scatter,ggplot2::aes(x=x,y=y)) +
+  ggplot2::geom_point()+
+  ggplot2::labs(x="xlab",y="ylab")+
+  ggplot2::ggtitle("Scatterplot Test")+
+  ggplot2::theme(text=ggplot2::element_text(size=10))+
+  ggplot2::geom_smooth(method = type, se = FALSE)
+helper_scatter_lm_type
 
 #test for input variables
 testthat::test_that("Function can only accept certain data types for arguments", {
@@ -24,6 +32,14 @@ testthat::test_that("Function can only accept certain data types for arguments",
   expect_error(scatter_plot(test_data_scatter, x, y, "x", "y", "title", "10"))
   
 })
+
+#test type of scatter_plot
+testthat::test_that("Function has the right type", {
+  expect_true(rlang::is_empty(type), helper_scatter_empty_type)
+  expect_true(type == "lm", helper_scatter_lm_type)
+})
+
+
 
 #test for output variables 
 testthat::test_that("Checks the output of the graph", {
