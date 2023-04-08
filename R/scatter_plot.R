8a400b65d73d44890eb08a8f9fa2dd2c8c7de64d
Jenna Le Noble <jenna_lenoble@hotmail.com>
2023-04-07 18:00:38 -0700

    updated README + scatterplot function

--- R/scatter_plot.R
+++ R/scatter_plot.R
@@ -4,7 +4,7 @@
 #'
 #' This function takes in a data set, a variable x from the data set,
 #' a variable y from the data set, an x-axis label, a y-axis label,
-#' a title for the plot and the text size.
+#' a title for the plot, the text size, and the model type (optional).
 #'
 #' @param data A data frame
 #' @param var1 the variable x in the relationship
@@ -13,18 +13,19 @@
 #' @param ylab the label of the y-axis
 #' @param title the title of the scatter plot with the regression line
 #' @param text_size the size of text of the labels/title
+#' @param type specify whether the model is linear with "lm" (default value = NULL)
 #'
 #' @return A scatter plot where
 #'   The x-axis are for the x variable specified
-#'   The x-axis are for the x variable specified
+#'   The y-axis are for the y variable specified
 #'
 #' @examples
-#' scatter_plot(mtcars, mpg, hp, "Miles Per Gallon", "Horse Power", "Scatter Plot for MPG vs HP", 10)
+#' scatter_plot(mtcars, mpg, hp, "lm", Miles Per Gallon", "Horse Power", "Scatter Plot for MPG vs HP", 10)
 #'
 #' @export
 #'
 
-scatter_plot <- function(data,var1,var2,xlab,ylab,title,text_size){
+scatter_plot <- function(data,var1,var2,xlab,ylab,title,text_size,type=NULL){
   x = dplyr::pull(data, {{var1}})
   y = dplyr::pull(data, {{var2}})
   if (!is.data.frame(data)){
@@ -49,12 +50,39 @@ scatter_plot <- function(data,var1,var2,xlab,ylab,title,text_size){
   else if(!is.numeric(text_size)){
     stop("Parameter text_size must be a numeric value.")
   }
+  else if(rlang::is_empty(type)){
+    return (
+      ggplot2::ggplot(data, ggplot2::aes(x=x,y=y)) +
+        ggplot2::geom_point()+
+        ggplot2::labs(x=xlab,y=ylab)+
+        ggplot2::ggtitle(title)+
+        ggplot2::theme(text=ggplot2::element_text(size=text_size))
+    )
+  }
+
+  else if(!is.character(type)){
+    stop("Please put in a string parameter for model type i.e. 'lm'.")
+  }
+
+  else if(type == "lm") {
+    return(
+      ggplot2::ggplot(data, ggplot2::aes(x=x,y=y)) +
+        ggplot2::geom_point()+
+        ggplot2::labs(x=xlab,y=ylab)+
+        ggplot2::ggtitle(title)+
+        ggplot2::theme(text=ggplot2::element_text(size=text_size))+
+        ggplot2::geom_smooth(method = type, se = FALSE)
+      )
+
+  }
+
   else {
     return (
       ggplot2::ggplot(data, ggplot2::aes(x=x,y=y)) +
         ggplot2::geom_point()+
         ggplot2::labs(x=xlab,y=ylab)+
         ggplot2::ggtitle(title)+
-        ggplot2::theme(text=ggplot2::element_text(size=text_size)))
+        ggplot2::theme(text=ggplot2::element_text(size=text_size))
+    )
   }
 }
