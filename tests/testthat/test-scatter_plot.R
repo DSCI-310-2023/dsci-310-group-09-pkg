e2239e2ddd2409c000c007674f885c2b8ac0ad49
Vera Danilova <veradanilova765@gmail.com>
2023-04-08 21:14:55 -0700

    added more tests

--- tests/testthat/test-scatter_plot.R
+++ tests/testthat/test-scatter_plot.R
@@ -10,7 +10,8 @@ helper_scatter_empty_type <- ggplot2::ggplot(test_data_scatter,ggplot2::aes(x=x,
   ggplot2::geom_point()+
   ggplot2::labs(x="xlab",y="ylab")+
   ggplot2::ggtitle("Scatterplot Test")+
-  ggplot2::theme(text=ggplot2::element_text(size=10))
+  ggplot2::theme(text=ggplot2::element_text(size=10)) + 
+  ggplot2:: geom_smooth(method = NULL, se = FALSE)
 helper_scatter_empty_type
 
 helper_scatter_lm_type <- ggplot2::ggplot(test_data_scatter,ggplot2::aes(x=x,y=y)) +
@@ -18,36 +19,47 @@ helper_scatter_lm_type <- ggplot2::ggplot(test_data_scatter,ggplot2::aes(x=x,y=y
   ggplot2::labs(x="xlab",y="ylab")+
   ggplot2::ggtitle("Scatterplot Test")+
   ggplot2::theme(text=ggplot2::element_text(size=10))+
-  ggplot2::geom_smooth(method = type, se = FALSE)
-helper_scatter_lm_type
+  ggplot2::geom_smooth(method = "lm", se = FALSE) 
+helper_scatter_lm_type  
+
 
 #test for input variables
 testthat::test_that("Function can only accept certain data types for arguments", {
-  expect_error(scatter_plot(c(1,0), c(1,0), c(0,1), "x", "y", "title", 10))
-  expect_error(scatter_plot(test_data_scatter2, x, y, "x", "y", "title", 10))
-  expect_error(scatter_plot(test_data_scatter3, x, y, "x", "y", "title", 10))
-  expect_error(scatter_plot(test_data_scatter, x, y, x, "y", "title", 10))
-  expect_error(scatter_plot(test_data_scatter, x, y, "x", y, "title", 10))
-  expect_error(scatter_plot(test_data_scatter, x, y, "x", "y", 10, 10))
-  expect_error(scatter_plot(test_data_scatter, x, y, "x", "y", "title", "10"))
-  
+  expect_error(scatter_plot(c(1,0), c(1,0), c(0,1), "x", "y", "title", 10, NULL))
+  expect_error(scatter_plot(test_data_scatter2, x, y, "x", "y", "title", 10, "lm"))
+  expect_error(scatter_plot(test_data_scatter3, x, y, "x", "y", "title", 10, NULL))
+  expect_error(scatter_plot(test_data_scatter, x, y, x, "y", "title", 10, NULL))
+  expect_error(scatter_plot(test_data_scatter, x, y, "x", y, "title", 10, NULL))
+  expect_error(scatter_plot(test_data_scatter, x, y, "x", "y", 10, 10, NULL))
+  expect_error(scatter_plot(test_data_scatter, x, y, "x", "y", "title", "10", NULL))
+  expect_error(scatter_plot(test_data_scatter, x, y, "x", "y", "title", 10, "NULL"))
+  expect_error(scatter_plot(test_data_scatter, x, y, "x", "y", "title", 10, lm))
 })
 
 #test type of scatter_plot
 testthat::test_that("Function has the right type", {
-  expect_true(rlang::is_empty(type), helper_scatter_empty_type)
-  expect_true(type == "lm", helper_scatter_lm_type)
+  expect_equal(helper_scatter_empty_type$type, NULL)
+  expect_equal(helper_scatter_lm_type$type,"lm")
 })
 
 
 
-#test for output variables 
+#test for output variables for function with type=NULL
 testthat::test_that("Checks the output of the graph", {
-  plot <- scatter_plot(test_data_scatter, x, y, "x", "y", "Scatterplot Test", 10)
-  expect_true(ggplot2::is.ggplot(plot))
-  expect_equal(ggplot2::xlab(plot)$label, ggplot2::xlab(test_data_scatter)$label)
-  expect_equal(ggplot2::ylab(plot)$label, ggplot2::ylab(test_data_scatter)$label)
-  expect_equal(ggplot2::ggtitle(plot)$label, ggplot2::ggtitle(test_data_scatter)$label)
-  
+  plot1 <- scatter_plot(test_data_scatter, x, y, "x", "y", "Scatterplot Test", 10, NULL)
+  expect_true(ggplot2::is.ggplot(plot1))
+  expect_equal(ggplot2::xlab(plot1)$label, ggplot2::xlab(test_data_scatter)$label)
+  expect_equal(ggplot2::ylab(plot1)$label, ggplot2::ylab(test_data_scatter)$label)
+  expect_equal(ggplot2::ggtitle(plot1)$label, ggplot2::ggtitle(test_data_scatter)$label)
+  expect_equal(ggplot2::plot1$type, NULL)
 })
 
+#test for output variables for function with type="lm"
+testthat::test_that("Checks the output of the graph", {
+  plot2 <- scatter_plot(test_data_scatter, x, y, "x", "y", "Scatterplot Test", 10, "lm")
+  expect_true(ggplot2::is.ggplot(plot2))
+  expect_equal(ggplot2::xlab(plot2)$label, ggplot2::xlab(test_data_scatter)$label)
+  expect_equal(ggplot2::ylab(plot2)$label, ggplot2::ylab(test_data_scatter)$label)
+  expect_equal(ggplot2::ggtitle(plot2)$label, ggplot2::ggtitle(test_data_scatter)$label)
+  expect_equal(ggplot2::type(plot2), type=="lm")
+})
